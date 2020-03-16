89202c2545818a9ac74277bdc88b9a12e5516d2a
Jozef Mravec <jojomravec@gmail.com>
2020-03-16 16:31:33 +0100

    Finally

--- GildedRose/Inventory.cs
+++ GildedRose/Inventory.cs
@@ -22,8 +22,66 @@ namespace GildedRose
         /// </summary>
         public void UpdateQuality()
         {
-            // TODO ...
-            // Hint: Iterate through this.items and check Name property to access specific item
+            foreach(Item item in items)
+            {
+                item.Quality--;
+                item.SellIn--;
+
+                if (item.Name == "Aged Brie")
+                {
+                    item.Quality+=2;                   
+                    if (item.SellIn < 0)
+                    {
+                        item.Quality +=2;
+                    }
+                }
+
+                if (item.SellIn < 0)
+                {
+                    item.Quality --;
+                }
+                if (item.Name.Contains("Conjured"))
+                {
+                    item.Quality--;
+                    if (item.SellIn < 0)
+                    {
+                        item.Quality--;
+                    }
+                }
+                if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    item.Quality += 2;
+
+                    if (item.SellIn < 11)
+                    {
+                        item.Quality++;
+                    }
+                    if (item.SellIn < 6)
+                    {
+                        item.Quality++;
+                    }
+                    if (item.SellIn <= 0)
+                    {
+                        item.Quality = 0;
+                    }
+                }
+
+                if (item.Quality > 50)
+                {
+                    item.Quality = 50;
+                }
+                if (item.Quality < 0)
+                {
+                    item.Quality = 0;
+                }
+
+                if (item.Name == "Sulfuras, Hand of Ragnaros")
+                {
+                    item.Quality = 80;
+                    item.SellIn++;
+                }
+                
+            }
         }
     }
 }
